36edf5adde330da0eb000b0ad98b5f2ce380efe0
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-08-07 11:00:17 +0530

    Create lab11.java

---
+++ lab11.java
@@ -0,0 +1,12 @@
+ public class bike1() 
+ {
+    bike1()
+    {
+        System.out.println("bike is created");
+    }
+    public static void main(String args[]);
+    {
+        bike1 b=new bike1();
+    }
+    
+}
