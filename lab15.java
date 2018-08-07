db8a9d9ab7f651d5ffe3764633a56802bb3641df
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-08-07 11:29:20 +0530

    Create lab15.java

---
+++ lab15.java
@@ -0,0 +1,25 @@
+public class counter2
+{
+   
+   static int count=0;
+    counter2()
+    {
+        count++;
+         System.out.println(count );
+        
+    }
+    public static void main(String  args[])
+    {
+    counter2  c1=new counter2();
+    counter2  c2=new counter2();
+    counter2  c3=new counter2();
+    
+}
+}
+
+
+
+
+
+
+
