677079b58b9c32b05087675a15a14cfbef3c3ee8
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-08-07 09:26:52 +0530

    Create lab2.java

---
+++ lab2.java
@@ -0,0 +1,9 @@
+public class student1{
+    int id;
+    String name;
+    public static void main(String[] args) {
+    student1 s1=new student1();
+		System.out.println("s1.id");
+		System.out.println("s1.name");
+	}
+}
