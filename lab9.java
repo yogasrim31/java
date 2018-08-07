f09a96f7f82b66674d2a76446c21e099c09ea077
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-08-07 10:34:32 +0530

    Create lab9.java

---
+++ lab9.java
@@ -0,0 +1,15 @@
+public class overloading1
+{
+    
+      public static void main(int a)
+      {
+          System.out.println(a);
+      }
+       public static void   main (String[] args) 
+       {
+             System.out.println("main() method invoked");
+             main(10);
+             }
+      
+}
+
