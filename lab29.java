475d8ef23918ad7e9823c7a76ddb9bb97c15b74d
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:40:47 +0530

    Create lab29.java

---
+++ lab29.java
@@ -0,0 +1,17 @@
+
+package javaset2;
+
+public class javalab029 {
+	void run(){System.out.println("Vehicle is running");}  
+		}  
+		class Bike extends javalab029{  
+		    
+	  public static void main(String args[]){  
+		  Bike obj = new Bike();  
+		  obj.run();  
+		  }  
+
+	
+
+}
+
