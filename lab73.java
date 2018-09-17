7598b839f64fb2db49dd3f73f10043cb0207d86c
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:56:06 +0530

    Create lab73.java

---
+++ lab73.java
@@ -0,0 +1,8 @@
+Ex:73                                                              SUBSTRING
+public class TestSubstring{  
+ public static void main(String args[]){  
+   String s="YOGASRIMUTHURAMAN";  
+   System.out.println(s.substring(6));
+   System.out.println(s.substring(0,6));  
+ }  
+}  
