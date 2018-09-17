6c714c538e357e2ae3756e83bcf1022ffdc36927
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:45:05 +0530

    Create lab42.java

---
+++ lab42.java
@@ -0,0 +1,14 @@
+
+class Animal{  
+ void eat(){System.out.println("animal is eating...");}  
+}  
+ 
+class Dog extends Animal{  
+ void eat(){System.out.println("dog is eating...");}  
+  
+ public static void main(String args[]){  
+  Animal a=new Dog();  
+  a.eat();  
+ }  
+}  
+
