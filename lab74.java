2a6cf973dbfd72cba15540bfed9af9dc043eb41f
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:56:22 +0530

    Create lab74.java

---
+++ lab74.java
@@ -0,0 +1,10 @@
+public class str{  
+public static void main(String args[]){  
+String s1="hello string";
+String s2="JAVATPOINT”; 
+String s1upper=s1.toUpperCase();  
+String s2lower=s1.toLowerCase();  
+
+System.out.println(s1upper);
+ System.out.println(s2lower);
+}}  
