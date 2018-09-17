695ca8381d3c8d657b056746634e720e85afbad0
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:43:05 +0530

    Create lab36.java

---
+++ lab36.java
@@ -0,0 +1,17 @@
+package javaset2;
+
+public class javalab036 {
+		 final void run(){System.out.println("running");}  
+		}  
+		     
+		class Honda extends javalab036{  
+		   void run(){System.out.println("running safely with 100kmph");}  
+		     
+		   public static void main(String args[]){  
+		   Honda honda= new Honda();  
+		   honda.run();  
+	   }  
+
+}
+
+
