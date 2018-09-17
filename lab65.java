57a4f0874fff116b94a2bfdb0269e2cce52c0fdc
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:53:33 +0530

    Create lab65.java

---
+++ lab65.java
@@ -0,0 +1,17 @@
+
+class A{  
+public static void main(String args[]){  
+  
+for(int i=0;i<args.length;i++)  
+System.out.println(args[i]);  
+  
+}  
+}  
+
+
+
+compile by > javac A.java  
+run by > java A sri yoga 1 3 sribbnb
+
+
+
