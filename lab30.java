dbe825f9d8038b1a4fe2a685f02ef8a0d9b563bc
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:41:09 +0530

    Create lab30.java

---
+++ lab30.java
@@ -0,0 +1,14 @@
+
+package javaset2;
+
+	class javalab030{  
+	void run(){System.out.println("Vehicle is running");}  
+	}  
+	class Bike2 extends javalab030{  
+	void run(){System.out.println("Bike is running safely");}  
+	  
+	public static void main(String args[]){  
+	Bike2 obj = new Bike2();  
+	obj.run();  
+	}
+	}
