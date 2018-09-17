3de475991caf4aed4c1e4bf98e1e06c5838a456c
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:41:28 +0530

    Create lab31.java

---
+++ lab31.java
@@ -0,0 +1,19 @@
+
+package javaset2;
+
+public class javalab031 {
+	int speed=50;  
+		}  
+		class Bike3 extends javalab031{  
+		  int speed=100;  
+		  void display(){  
+		   System.out.println(speed);//will print speed of Bike   
+		  }  
+		  public static void main(String args[]){  
+		   Bike3 b=new Bike3();  
+		   b.display();  
+		}  
+
+
+}
+
