fa2683c4f7fe44e31f9826ec9b684ce9f70db766
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:47:24 +0530

    Create lab50.java

---
+++ lab50.java
@@ -0,0 +1,24 @@
+interface A{  
+void a();//bydefault, public and abstract  
+void b();  
+void c();  
+void d();  
+}  
+abstract class B implements A{  
+public void c(){System.out.println("I am C");}  
+}  
+  
+class M extends B{  
+public void a(){System.out.println("I am a");}  
+public void b(){System.out.println("I am b");}  
+public void d(){System.out.println("I am d");}  
+}  
+  class Test5{  
+public static void main(String args[]){  
+A a=new M();  
+a.a();  
+a.b();  
+a.c();  
+a.d();  
+}}  
+
