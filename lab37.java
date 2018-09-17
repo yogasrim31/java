164f27323631ee3311e7f298b3c95d12669950b5
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:43:22 +0530

    Create lab37.java

---
+++ lab37.java
@@ -0,0 +1,16 @@
+
+package javaset2;
+
+public class javalab037 {
+		class Honda1 extends javalab037{  
+			  void run(){System.out.println("running safely with 100kmph");}  
+			    
+			  public void main(String args[]){  
+			  Honda honda= new Honda();  
+		  honda.run();  
+		  }  
+
+
+		}
+}
+
