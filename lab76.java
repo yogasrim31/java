c19df7dffca1dd41dc212e17ff1dc6a10995a630
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:56:58 +0530

    Create lab76.java

---
+++ lab76.java
@@ -0,0 +1,7 @@
+
+public class StartsWithExample{  
+public static void main(String args[]){  
+String s="kailash";  
+System.out.println(s.startsWith("ka"));  
+System.out.println(s.endsWith("sh"));  
+}}  
