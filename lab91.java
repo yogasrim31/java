184109ff2ca85fdac1dbccc930eee8cb65c2afc9
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 21:02:36 +0530

    Create lab91.java

---
+++ lab91.java
@@ -0,0 +1,10 @@
+
+import java.util.StringTokenizer;  
+public class Simple{  
+ public static void main(String args[]){  
+   StringTokenizer st = new StringTokenizer("my name is yogasri"," ");  
+     while (st.hasMoreTokens()) {  
+         System.out.println(st.nextToken());  
+     }  
+   }  
+}  
