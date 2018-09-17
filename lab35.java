8d6a338a15340306bf4e51838460f556a98cdc1e
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:42:44 +0530

    Create lab35.java

---
+++ lab35.java
@@ -0,0 +1,16 @@
+
+package javaset2;
+
+public class javalab035 {
+		 int speedlimit=90;//final variable  
+		 void run(){  
+		  speedlimit=400;  
+		 }  
+		 public static void main(String args[]){  
+		 javalab035 obj=new javalab035();  
+		 obj.run();  
+		 }  
+
+
+}
+
