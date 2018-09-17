0b61d7ce5b8e14c606d892cb3b085e938ad7317f
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:43:41 +0530

    Create lab38.java

---
+++ lab38.java
@@ -0,0 +1,15 @@
+
+class Honda2{  
+	  final void run(){System.out.println("running...");}  
+	}  
+	class Exp38 extends Honda2{  
+	   public static void main(String args[]){  
+	    new Exp38().run();  
+	   }  
+	}  
+
+
+
+
+
+
