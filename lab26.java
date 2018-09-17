5be81cb4986faccaebf26f5ddbb947ad7812d342
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:36:15 +0530

    Create lab26.java

---
+++ lab26.java
@@ -0,0 +1,16 @@
+package javaset2;
+ class sri{
+	float salary=40000;  
+		}  
+		class javalab026 extends sri {  
+		 int bonus=10000;  
+		 public static void main(String args[]){  
+			 javalab026 p=new javalab026();  
+	   System.out.println("Programmer salary is:"+p.salary);  
+		   System.out.println("Bonus of Programmer is:"+p.bonus);  
+		}  
+
+	
+
+}
+
