65b6952cab6c5a7c1d8c06ffbdb81b7ea4732427
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 21:00:22 +0530

    Create lab86.java

---
+++ lab86.java
@@ -0,0 +1,8 @@
+
+class StringBuilderExample5{  
+public static void main(String args[]){  
+StringBuilder sb=new StringBuilder(“yoga");  
+sb.reverse();  
+System.out.println(sb);//prints olleH  
+}  
+}  
