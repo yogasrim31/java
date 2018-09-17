7470c8cbcdc963d9f1a273fe3bb1d3a419e68646
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:58:28 +0530

    Create lab81.java

---
+++ lab81.java
@@ -0,0 +1,7 @@
+
+     public class ReplaceExample1{  
+public static void main(String args[]){  
+String s1="manisha";  
+String replaceString=s1.replace('a','g');  
+System.out.println(replaceString);  
+}}  
