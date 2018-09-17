47c53ddcb3e8630a68cc6da8c79b4a8c2eb5461b
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:54:07 +0530

    Create lab67.java

---
+++ lab67.java
@@ -0,0 +1,8 @@
+
+class Testimmutablestring{  
+ public static void main(String args[]){  
+   String s="Tony";  
+   s.concat(" Stark");//concat() method appends the string at the end  
+   System.out.println(s);//will print Sachin because strings are immutable objects  
+ }  
+}  
