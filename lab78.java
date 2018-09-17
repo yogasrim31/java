afd6efe97cce328768dd46f055cfa82eead94c46
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:57:28 +0530

    Create lab78.java

---
+++ lab78.java
@@ -0,0 +1,8 @@
+
+public class LengthExample{  
+public static void main(String args[]){  
+String s1="vijayaraghavan";  
+String s2="sri";  
+System.out.println("string length is: "+s1.length());  
+System.out.println("string length is: "+s2.length());  
+}}  
