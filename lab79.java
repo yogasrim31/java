8474f6dfa698b711382ae8e9974aa01401185d4d
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:57:57 +0530

    Create lab79.java

---
+++ lab79.java
@@ -0,0 +1,9 @@
+public class InternExample{  
+public static void main(String args[]){  
+String s1=new String("hello");  
+String s2="hello";  
+String s3=s1.intern();   
+System.out.println(s1==s2);  
+System.out.println(s2==s3);
+}}  
+
