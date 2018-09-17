5ecfed167c5ecd75bbbd9fbeb5ce1426240882b2
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-17 20:58:43 +0530

    Create lab82.java

---
+++ lab82.java
@@ -0,0 +1,9 @@
+
+class StringBuilderExample{  
+public static void main(String args[]){  
+StringBuilder sb=new StringBuilder("Hello ");  
+sb.append("Java");
+System.out.println(sb);//prints Hello Java  
+}  
+}  
+
