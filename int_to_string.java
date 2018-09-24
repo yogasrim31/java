0d2770a45aacf02d5f437d3d21e249abf4ffece6
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-24 20:37:07 +0530

    Create int_to_string.java

---
+++ int_to_string.java
@@ -0,0 +1,10 @@
+public class int_to_string 
+{
+public static void main(String args[])
+{
+	int a = 400;
+	String str = String.valueOf(a);
+	System.out.println(a + 150);
+	System.out.println(str +" "+ 150);
+}
+}
