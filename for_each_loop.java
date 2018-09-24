e20d6ee8c4840ed4853bc2edc16ba7ecbd0e6942
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-24 20:42:17 +0530

    Create  for_each_loop.java

---
+++ for_each_loop.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class for_each_loop 
+{
+public static void main(String args[])
+{
+	ArrayList<String> a = new ArrayList<String>();  
+	  a.add("yoga");  
+	  a.add("meenu");  
+	  a.add("swetha");  
+	  a.add("ippu");  
+	  for(String str:a)  
+	  System.out.println(str);  
+
+}
+}
