4343495c85a7d9d81abfd0b65e3237ab3f07feac
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-24 20:37:36 +0530

    Create int_to_string2 .java

---
+++ int_to_string2 .java
@@ -0,0 +1,10 @@
+public class int_to_string2 
+{
+public static void main(String args[])
+{
+	int a = 300;
+	String str = Integer.toString(a);
+	System.out.println(a+150);
+	System.out.println(str + " "+ 150);
+}
+}
