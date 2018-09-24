4665e9ee074532ea3144843b3db09aa09abf519a
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-24 20:36:38 +0530

    Create string_to_int .java

---
+++ string_to_int .java
@@ -0,0 +1,10 @@
+public class string_to_int 
+{
+public static void main(String args[])
+{
+	String a = "300";
+	int x = Integer.parseInt(a);
+	System.out.println(a +" " + 150);
+	System.out.println(x + 150);
+}
+}
