28d87db4ee5bd75f266b08fc281f0f6ad10d82cb
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-25 09:51:31 +0530

    Create FileOutputStreamExample .java

---
+++ FileOutputStreamExample .java
@@ -0,0 +1,14 @@
+import java.io.FileOutputStream;  
+public class FileOutputStreamExample {  
+    public static void main(String args[]){    
+           try{    
+             FileOutputStream fout=new FileOutputStream("D:\\abc.txt");    
+             String s="Dhoni is my favourite player";    
+             byte b[]=s.getBytes();//converting string into byte array    
+             fout.write(b);    
+             fout.close();    
+             System.out.println("success...");    
+            }catch(Exception e){System.out.println(e);}    
+      }    
+}  
+
