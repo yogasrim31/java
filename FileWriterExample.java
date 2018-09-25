20898ab0d627b385113dce6b917fabb5112a12b0
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-25 09:54:03 +0530

    Create FileWriterExample.java

---
+++ FileWriterExample.java
@@ -0,0 +1,13 @@
+package com.javatpoint;  
+import java.io.FileWriter;  
+public class FileWriterExample {  
+    public static void main(String args[]){    
+         try{    
+           FileWriter fw=new FileWriter("D:\\testout.txt");    
+           fw.write("this is file writer");    
+           fw.close();    
+          }catch(Exception e){System.out.println(e);}    
+          System.out.println("Success...");    
+     }    
+}  
+
