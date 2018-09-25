f4064351c109d33e55a2ee35b5bd345db4fbc077
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-09-25 09:51:53 +0530

    Create class FilterExample .java

---
+++ class FilterExample .java
@@ -0,0 +1,14 @@
+import java.io.*;  
+public class FilterExample {  
+    public static void main(String[] args) throws IOException {  
+        File data = new File("D:\\testout.txt");  
+        FileInputStream  file = new FileInputStream(data);  
+        FilterInputStream filter = new BufferedInputStream(file);  
+        int k =0;  
+        while((k=filter.read())!=-1){  
+            System.out.print((char)k);  
+        }  
+        file.close();  
+        filter.close();  
+    }  
+}  
